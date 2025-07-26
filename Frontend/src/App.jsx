e7fc7fafec337359b58b5c14defe402f869cb876
alan-srivastava <141805142+alan-srivastava@users.noreply.github.com>
2025-07-26 14:19:24 +0530

    Connect frontend to deployed backend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-   const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+   const response = await axios.post('https://ai-powered-code-reviewer-pro-43x5.onrender.com/', { code })
     setReview(response.data)
 }
   return (
